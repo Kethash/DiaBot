82ec31c2dbf7a8ffcbd3e167b7714f581f0826d1
Kethash <61096082+Kethash@users.noreply.github.com>
2022-08-07 21:51:43 +0200

    Made /about command
    + Minor fixes

---
+++ src/commands/about.ts
@@ -0,0 +1,27 @@
+import { EmbedBuilder, SlashCommandBuilder, User } from 'discord.js';
+
+export = {
+    data: new SlashCommandBuilder()
+            .setName('about')
+            .setDescription('About Dia saaan !'),
+    async execute(interaction: any) {
+
+        const botAuthor: User = await interaction.guild.members.cache.get('818224462809006103')
+
+        const exampleEmbed = new EmbedBuilder()
+            .setColor('#FD5E53')
+            .setTitle('About me')
+            .setURL('https://discord.js.org/')
+            .setAuthor({ name: 'Dia Kurosawa'})
+            .setDescription("I'm Dia Kurosawa !\nI'm here to bring order in your server and bust LoveLeavers !")
+            .setThumbnail('https://pbs.twimg.com/profile_images/1471849547659988992/KdQh_Th2_400x400.jpg')
+            .addFields(
+                { name: 'GitHub project link', value: 'https://github.com/Kethash/DiaBot' },
+                { name: 'Developper', value: botAuthor.toString() }
+            )
+            .setTimestamp(new Date());
+        
+        await interaction.reply({ embeds: [exampleEmbed], ephemeral: true });
+        
+    }
+}
--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -4,7 +4,7 @@ export = {
     data: new SlashCommandBuilder()
             .setName('ping')
             .setDescription('Replies with Pong!'),
-    async execute(interaction: { reply: (arg0: string) => any; }) {
-        await interaction.reply('Pong! test');
+    async execute(interaction: { reply: (arg0: Object) => any; }) {
+        await interaction.reply({content: 'Pong! test', ephemeral: true});
     }
 }
