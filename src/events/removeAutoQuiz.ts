7dabdb00ada0ec06c23e3e245969c2ddf79af800
Kethash <61096082+Kethash@users.noreply.github.com>
2023-01-01 19:16:15 +0100

    Can now remove json imported quiz
    Added an import quizz rejection if we need

---
+++ src/events/removeAutoQuiz.ts
@@ -0,0 +1,14 @@
+import { Events, Interaction } from "discord.js";
+
+export = {
+    name: Events.InteractionCreate,
+    async execute(redisClient: any, interaction: Interaction) {
+        if (!interaction.isStringSelectMenu()) return;
+        if (!(interaction.customId == 'selectquiz')) return;
+
+        const quizzName = interaction.values[0];
+        await redisClient.json.del(quizzName);
+        await interaction.reply({content: 'Quizz removed successfully', ephemeral: true});
+        
+    }
+}
