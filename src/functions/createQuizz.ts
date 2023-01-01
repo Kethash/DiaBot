7316799d6a67bfdda46d64d6d30ef819a5ce0ede
Kethash <61096082+Kethash@users.noreply.github.com>
2023-01-01 16:05:09 +0100

    Hotfix
    createQuizz not working

--- src/functions/createQuizz.ts
+++ src/functions/createQuizz.ts
@@ -1,18 +1,20 @@
+import axios from "axios";
 import { AttachmentBuilder, EmbedBuilder, TextChannel } from "discord.js";
+import sharp from "sharp";
 
 export async function createQuizzMessage(quizzName:string, userId: string, channel: TextChannel, redisClient: any): Promise<void> {
-    const quizzs: Array<{imageLink: string, answers: string}> = await redisClient.json.get(quizzName, {path: '.quizzs'});
+    const quizzs: Array<{title: string, imageLink: string, blurImage: boolean, answers: string}> = await redisClient.json.get(quizzName, {path: '.quizzs'});
     
     const question = quizzs[Math.floor(Math.random() * quizzs.length)];
     
-    const options = {};
+    const options: { embeds?: EmbedBuilder[] } = {};
     
     if (question.imageLink && (question?.blurImage === true || question?.blurImage)) {
         const buffer = (await axios.get(question.imageLink, { responseType: 'arraybuffer' })).data as Buffer;
         console.log(buffer);
         let blurredImageBuffer = await sharp(buffer).blur(45).toBuffer();
         
-        const imageFileName = question.imageLink.substring(question.imageLink.lastIndexOf('/') + 1));
+        const imageFileName = question.imageLink.substring(question.imageLink.lastIndexOf('/') + 1);
         console.log(imageFileName);
         let blurredImageAttachment = new AttachmentBuilder(buffer, { name: imageFileName });
         console.log(blurredImageAttachment);
