506bbd6c09573a66d9a39114240bcb554d921bcd
Kethash <61096082+Kethash@users.noreply.github.com>
2023-01-01 16:55:34 +0100

    Update createQuizz.ts
    Use the blurRate from the json

--- src/functions/createQuizz.ts
+++ src/functions/createQuizz.ts
@@ -3,7 +3,7 @@ import { AttachmentBuilder, EmbedBuilder, TextChannel } from "discord.js";
 import sharp from "sharp";
 
 export async function createQuizzMessage(quizzName:string, userId: string, channel: TextChannel, redisClient: any): Promise<void> {
-    const quizzs: Array<{title: string, imageLink: string, blurImage: boolean, answers: string}> = await redisClient.json.get(quizzName, {path: '.quizzs'});
+    const quizzs: Array<{title: string, imageLink: string, blurImage: boolean, blurRate: number, answers: string}> = await redisClient.json.get(quizzName, {path: '.quizzs'});
     
     const question = quizzs[Math.floor(Math.random() * quizzs.length)];
     
@@ -12,7 +12,7 @@ export async function createQuizzMessage(quizzName:string, userId: string, chann
     if (question.imageLink) {
         let buffer = (await axios.get(question.imageLink, { responseType: 'arraybuffer' })).data as Buffer;
         // console.log(buffer);
-        if (question?.blurImage === true) buffer = await sharp(buffer).blur(30).toBuffer();
+        if (question?.blurImage === true) buffer = await sharp(buffer).blur(question.blurRate).toBuffer();
         
         const imageFileName = question.imageLink.substring(question.imageLink.lastIndexOf('/') + 1);
         // console.log(imageFileName);
