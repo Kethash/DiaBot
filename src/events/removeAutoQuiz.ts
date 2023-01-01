c5f4addf2ff643cb23c2272152fc58a42fc71220
Kethash <61096082+Kethash@users.noreply.github.com>
2023-01-01 20:00:48 +0100

    Update removeAutoQuiz.ts

--- src/events/removeAutoQuiz.ts
+++ src/events/removeAutoQuiz.ts
@@ -4,7 +4,7 @@ export = {
     name: Events.InteractionCreate,
     async execute(redisClient: any, interaction: Interaction) {
         if (!interaction.isStringSelectMenu()) return;
-        if (!(interaction.customId == 'selectquiz')) return;
+        if (!(interaction.customId == 'removequizz')) return;
 
         const quizzName = interaction.values[0];
         await redisClient.json.del(quizzName);
