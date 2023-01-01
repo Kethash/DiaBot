558c0248c98a56177d66e914e66a84509a81c343
Tom Berriot <tom51100@live.fr>
2023-01-01 14:38:09 +0100

    Fix emojis in message on quizz validation

--- src/events/onQuizzAnswer.ts
+++ src/events/onQuizzAnswer.ts
@@ -8,50 +8,44 @@ export = {
     async execute(redisClient: any, message: Message) {
         if (message.author.bot) return;
         if (!(message.type === MessageType.Reply)) return;
-        // console.log(message);
         
         const repliedTo = await message.channel.messages.fetch(message.reference?.messageId as string);
-        // console.log('REPLIED TO', repliedTo);
         
         if (repliedTo.author.id !== config.clientID) return;
 
         let answer = await redisClient.json.get(`answer:${message.reference?.messageId}`, '.');
-        
-        // console.log('ANSWER', answer);
 
         let successToAnswer = answer.answers.split(';').some((answerString: string) => compareAnswers(message.content, answerString, answer?.isStrict ?? false));
-        // console.log(successToAnswer);
-        // ぼく　は　Liyuu が　とても　すきです。
 
         let noEmotes = [
-            ':Hanano:1058191475508256828',
-            ':Hanayokotta:1058191475508256828',
-            ':Bubudesuwa:1058191475508256828',
-            ':DiaSmile:1058191475508256828',
-            ':UmiPokerFace:1058191475508256828',
-            ':ayumunohope:1058191475508256828',
-            ':WutHonoka:1058191475508256828',
-            ':youdisapointed:1058191475508256828',
-            ':yuonegai:1058191475508256828'
+            '<:Hanano:964268713740222484>', 
+            '<:Hanayokotta:964268716781101086>',
+            '<:Bubudesuwa:809220132319526932>',
+            '<:DiaSmile:827687467175903252>',
+            '<:UmiPokerFace:809208838291980348>',
+            '<:ayumunohope:983437653628293201>',
+            '<:WutHonoka:809208443344912385>',
+            '<:youdisapointed:854422826626056192>',
+            '<:yuonegai:983437684859097140>'
         ];
 
         let yesEmotes = [
-            ':YouThumbsUp:1058191475508256828',
-            ':DiaLUL:1058191475508256828',
-            ':UmiHappy:1058191475508256828',
-            ':kanatablade:1058191475508256828',
-            ':lanzhuproud:1058191475508256828',
-            ':MariJoke:1058191475508256828',
-            ':SetsunaFire:1058191475508256828',
-            ':rubycool:1058191475508256828',
-            ':eliapprove:1058191475508256828'
+            '<:YouThumbsUp:864644229539299328>',
+            '<:DiaLUL:809220806331990037>',
+            '<:UmiHappy:974431334330142750>',
+            '<:kanatablade:983426928109318206>',
+            '<:lanzhuproud:983437667033317466>',
+            '<:MariJoke:809219775451627591>',
+            '<:SetsunaFire:809221805985497138>',
+            '<:rubycool:853950147917381672>',
+            '<:eliapprove:964271606379724943>'
         ];
 
         const yesEmote = yesEmotes[Math.floor(Math.random() * yesEmotes.length)];
         const noEmote = yesEmotes[Math.floor(Math.random() * yesEmotes.length)];
 
-        if (successToAnswer) message.reply(`<${yesEmote}> Correct! ❤ <${yesEmote}>`)
-        else message.reply(`<${noEmote}> BUU BUU DESUWA ! <${noEmote}>\n Corrects answers were : \n ${answer.answers.replace(';', '\n - ')} `)
+        if (successToAnswer) message.reply(`${yesEmote} Correct ! ❤ ${yesEmote}`)
+        else message.reply(`${noEmote} BUU BUU DESUWA ! ${noEmote}\n Corrects answers were : \n - ${answer.answers.replace(';', '\n - ')} `)
 
         // Suppression clef
         await redisClient.json.del(`answer:${message.reference?.messageId}`, '.');
