41c899ffadff3cac47a1107d475a10c3379528ab
Tom Berriot <tberriot@rosaly.com>
2023-01-04 23:32:47 +0100

    Escape ,

--- src/functions/answer-parsing.ts
+++ src/functions/answer-parsing.ts
@@ -3,7 +3,7 @@ import FuzzySet from 'fuzzyset';
 export function formatString(string: string): string {
     string = string.trim();
     string = string.replace(/\s/g, '')
-    const charsToRemove = ['.','~','?','・','!','*','&','!','→',':','-','♡','(',')','μ','?','！','☆','☆'];
+    const charsToRemove = ['.', ',', '~', '?', '・', '!', '*', '&', '!', '→', ':', '-', '♡', '(', ')', 'μ', '?', '！', '☆', '☆'];
     charsToRemove.forEach((char) => string = string.replace(char, ''))
     return string.toLowerCase();
 }
@@ -12,14 +12,14 @@ export function formatString(string: string): string {
 export function compareAnswers(answer1: string, answer2: string, isStrict: boolean): boolean {
     let answerFormatted1 = formatString(answer1)
     let answerFormatted2 = formatString(answer2)
-    
+
     // console.log('answerformatted1', answerFormatted1);
     // console.log('answerformatted2', answerFormatted2);
-    
+
     const fuzzySet = FuzzySet([answerFormatted1]);
     // console.log(fuzzySet);
     // console.log('FUZZYSET', fuzzySet.get(answerFormatted2));
-    
+
     if (isStrict) {
         return answerFormatted1 === answerFormatted2;
     } else {
