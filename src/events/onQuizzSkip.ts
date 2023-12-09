d160bb16d5a4b25a0a5c0f05b430f388073870e6
Kethash <61096082+Kethash@users.noreply.github.com>
2023-12-09 18:04:49 +0100

    Replaced quizz reaction skipping to a skip button
    No longer need to use collectors

--- src/events/onQuizzSkip.ts
+++ src/events/onQuizzSkip.ts
@@ -1,4 +1,4 @@
-import { Events, TextChannel, Interaction , User } from "discord.js";
+import { Events, TextChannel, Interaction , User, ActionRowBuilder, ButtonBuilder, ButtonStyle } from "discord.js";
 import { sendQuizzMessage, replyQuizzAnswer } from "../functions/quizz";
 import config from "../../config.json"
 
@@ -6,13 +6,11 @@ export = {
     name: Events.InteractionCreate,
     async execute(redisClient: any, interaction: Interaction, user: User) {
 
-        console.log("triggered !");
         // Was reaction created by the bot
         if (interaction.user.bot) return;
         if (!interaction.isButton()) return;
         if (interaction.customId !== "skip") return;
-        console.log("triggered 2 !");
-
+        
         let answer = await redisClient.json.get(`answer:${interaction.message.id}`, '.');
         if (answer == null) return; // Prevent from multiple responses when players play together
 
@@ -25,10 +23,20 @@ export = {
 
         const channel: TextChannel = interaction.message.channel as TextChannel;
 
+        const disabledButton: any = new ActionRowBuilder()
+        .addComponents(
+            new ButtonBuilder()
+                .setCustomId('skip')
+                .setEmoji('⏭️')
+                .setStyle(ButtonStyle.Primary)
+                .setDisabled(true)
+        );
+
+        await interaction.update({components: [disabledButton]})
+
         await replyQuizzAnswer(false, answer, interaction.message);
         await sendQuizzMessage(answer.quizz_id, answer.author_id, channel, redisClient, null);
 
-        await interaction.deferUpdate();
         return;
     }
 }
