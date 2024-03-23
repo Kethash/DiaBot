fcd0c577f0a6dbcd15aa4db480399d380826417d
Kethash <61096082+Kethash@users.noreply.github.com>
2024-03-23 19:26:55 +0100

    Update onBotJoin.ts

--- src/events/onBotJoin.ts
+++ src/events/onBotJoin.ts
@@ -3,14 +3,17 @@ import { Collection, EmbedBuilder, Events, GuildMember, Role, Snowflake, TextCha
 export = {
     name: Events.GuildMemberAdd,
     async execute(member: GuildMember): Promise<void> {
-        setTimeout(() => {
-            if (member.guild.id !== "584018480853155842" || member.guild.id === null) return;
+        setTimeout(async () => {
+            if (!(await member.guild.members.fetch(member.id))) return;
+            // if (member.guild.id !== "584018480853155842" || member.guild.id === null) return;
             const roles: Collection<Snowflake, Role> = member.roles.valueOf();
             // Check if the roles are [Tomoriru, Belgique, Membre]
-            const number: boolean = roles.size === 3;
+            const number: boolean = roles.size == 3;
             // Hard coded because I am too lazy to include this in Redis 
-            const hasRoles: boolean = roles.every((val) => ["Tomoriru", "Belgique", "Membre"].includes(val.name));
-            if (hasRoles && number) {
+            // const hasRoles: boolean = roles.some((val) => ["Tomoriru", "Belgique", "Membre"].includes(val.name));
+            const hasRoles: boolean = ["Tomoriru", "Belgique", "Membre"].every(val => roles.find((key) => key.name === val));
+
+            if ([hasRoles, number].every(e => e === true)) {
                 const channel: TextChannel = member.guild.channels.cache.get("807908826840825856") as TextChannel;
                 const botSpottedEmbed: EmbedBuilder = new EmbedBuilder()
                                                             .setTitle('Bot ?')
